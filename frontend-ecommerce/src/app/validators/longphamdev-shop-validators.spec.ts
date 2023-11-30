b0405b712b623fb99462f58e4d010c5743d2a4a7
longphamdev <longpham16072001@gmail.com>
2023-11-30 16:19:12 +0700

    handle validation for checkout form

---
+++ frontend-ecommerce/src/app/validators/longphamdev-shop-validators.spec.ts
@@ -0,0 +1,7 @@
+import { LongphamdevShopValidators } from './longphamdev-shop-validators';
+
+describe('LongphamdevShopValidators', () => {
+  it('should create an instance', () => {
+    expect(new LongphamdevShopValidators()).toBeTruthy();
+  });
+});
